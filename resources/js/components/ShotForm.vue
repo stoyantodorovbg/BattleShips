8b6fb11544844b7d446a8358a9f7dc51ae5ed45e
stoyantodorovbg <stoyan_11@abv.bg>
2019-05-15 07:04:48 +0300

    Filtering of the wrong inputs

--- resources/js/components/ShotForm.vue
+++ resources/js/components/ShotForm.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="container">
+    <div class="container shot-form">
         <div class="row mt-5">
             <div class="col-6 input-group mb-3">
                 <div class="input-group-prepend">
                     <span class="input-group-text">ROW</span>
                 </div>
                 <input type="text"
-                       v-model="row_data"
+                       v-model="rowData"
                        maxlength="1"
                        class="form-control"
                        placeholder="Enter a Character"
@@ -15,7 +15,7 @@
             <div class="col-6 input-group mb-3">
 
                 <input type="number"
-                       v-model="col_data"
+                       v-model="colData"
                        maxlength="1"
                        min="1"
                        :max="getColsCount()"
@@ -31,10 +31,16 @@
             <div class="col-12 input-group mb-3">
                 <button @click="submitShot()"
                         type="button"
+                        :disabled="wrongInput ? true : false"
                         class="btn btn-primary w-100 font-weight-bolder"
                 >
                     SHOT
                 </button>
+            </div>
+        </div>
+        <div v-if="wrongInput" class="row font-weight-bolder wrong-input">
+            <div class="col-12 text-center">
+                WRONG INPUT!
             </div>
         </div>
     </div>
@@ -46,26 +52,53 @@
 
         data() {
             return {
-                row_data: '',
-                col_data: '',
+                rowData: '',
+                colData: '',
+                wrongInput: false,
+                rowsCount: this.getRowsCount(),
+                colsCount: this.getColsCount(),
             };
+        },
+
+        watch: {
+            rowData: function (value) {
+                this.wrongInput = false;
+
+                if(! this.checkRowData(value)) {
+                    this.wrongInput = true;
+                }
+            },
+            colData: function (value) {
+                this.wrongInput = false;
+
+                if(! this.checkColData(value)) {
+                    this.wrongInput = true;
+                }
+            }
         },
 
         methods: {
             submitShot() {
-                let component = this;
-                axios.post('/shot', this.getData())
-                    .then(response => {
-                        component.$parent.$data.gridData = response.data.grid
-                    }).catch(function () {
+                let data = this.getData();
+
+                if(data.row && data.col) {
+                    let component = this;
+
+                    axios.post('/shot', data)
+                        .then(response => {
+                            component.$parent.$data.gridData = response.data.grid
+                        }).catch(function () {
                         console.log('Something went wrong.');
-                });
+                    });
+                } else {
+                    this.wrongInput = true;
+                }
             },
 
             getData() {
                 return {
                     row: this.convertChar(),
-                    col: this.col_data,
+                    col: this.colData,
                 }
             },
 
@@ -73,12 +106,44 @@
                 return Object.keys(this.$parent.gridData[1]).length;
             },
 
+            getRowsCount() {
+                return Object.keys(this.$parent.$data.gridData).length;
+            },
+
             convertChar() {
                 let component = this;
                 return this.$parent.$data.alphabet.findIndex(function (char) {
-                    return char === component.row_data;
+                    return char === component.rowData.toLowerCase();
                 }) + 1;
+            },
+
+            checkRowData(value) {
+                let index = this.$parent.$data.alphabet.findIndex(function (element) {
+                    return element === value.toLowerCase();
+                });
+
+                if(index <= this.rowsCount && value.match(/[A-Za-z]/)) {
+                    return true;
+                }
+
+                return false;
+            },
+
+            checkColData(value) {
+                if(value <= this.colsCount) {
+                    return true;
+                }
+
+                return false;
             },
         }
     }
 </script>
+
+<style lang="scss">
+    .shot-form {
+        .wrong-input {
+            color: red;
+        }
+    }
+</style>
